96d882a6cf96228ebeecab074250615ad998b8c6
Titilope Apuabi <tyaps002@gmail.com>
2022-12-03 19:31:07 +0100

    Create greeting table

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# hello-rails-back-end
-This is a simple web application that create a React front-end with a Rails back-end, connecting them to display a random message.
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+greetings1 = Greeting.create([text: "Holla"])
+greetings2 = Greeting.create([text: "Hello"])
+greetings3 = Greeting.create([text: "Hi"])
+greetings4 = Greeting.create([text: "Hey"])
+greetings5 = Greeting.create([text: "Howdy"])
